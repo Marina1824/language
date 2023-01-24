8879c56de1247b43dcf8c3da61b1b53368bb2c82
Marina1824 <9mariam7@gmail.com>
2023-01-24 20:11:25 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,9 +1,7 @@
 from selenium.webdriver.common.by import By
 import time
 
-
-
 def test_basket(browser):
     time.sleep(5)
-    browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
-
+    buttons=browser.find_elements(By.CSS_SELECTOR, "button.btn-add-to-basket")
+    assert len(buttons) == 1, "Кнопка не найдена или их больше 1-й"
